5ac68fa6992abafb6868b9390bdfbf34e1338039
Jean-Louis <jeanlouis@peter-bertilssons-macbook-pro-2.local>
2010-02-09 14:44:30 +0100

    Added Unit test and improved angle computation

--- AugmentedPOI.m
+++ AugmentedPOI.m
@@ -16,15 +16,34 @@
 -(id)initWithAnnotation:(MPNAnnotation *) anAnnotation fromOrigin:(CLLocationCoordinate2D)origin
 {
 	self.annotation = anAnnotation;
-	self.teta = atan2((origin.latitude - anAnnotation.coordinate.latitude) * (110.574 + 0.562 * abs(origin.latitude / 90)),
-					  (origin.longitude - anAnnotation.coordinate.longitude) * 111.320 * cos(origin.latitude * 3.14 / 180));
+	[self updateAngleFrom:origin];
+	
 	return self;
 }
 
 -(void)updateAngleFrom:(CLLocationCoordinate2D)origin
 {
-	self.teta = atan2((origin.latitude - annotation.coordinate.latitude)* (110.574 + 0.562 * abs(origin.latitude / 90)),
-					  (origin.longitude - annotation.coordinate.longitude) * 111.320 * cos(origin.latitude * 3.14 / 180));
+	
+	float lat1 = origin.latitude * 3.14 / 180.0;
+	float lon1 = origin.longitude * 3.14 / 180.0;
+	
+	float lat2 = annotation.coordinate.latitude * 3.14 / 180.0;
+	float lon2 = annotation.coordinate.longitude * 3.14 / 180.0;
+	
+	if(lat1 == lat2 && lon1 == lon2){
+		teta = 0.0;
+	}else{
+		float dLon = lon2-lon1; 
+		
+		float y = sin(dLon) * cos(lat2);
+		float x = cos(lat1) * sin(lat2) - sin(lat1) * cos(lat2) * cos(dLon);
+		
+		teta = atan2(-x, -y);
+	}
+	/*
+	 self.teta = atan2((origin.latitude - annotation.coordinate.latitude)* (110.574 + 0.562 * abs(origin.latitude / 90)),
+	 (origin.longitude - annotation.coordinate.longitude) * 111.320 * cos(origin.latitude * 3.14 / 180));
+	 */
 }
 
 - (void)dealloc {
