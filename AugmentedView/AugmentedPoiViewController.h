50d03396d0c2e6aa04b63ce33fea7444a0690879
Jean-Louis <jeanlouis@rnd-delta.dialog.se>
2010-03-02 11:29:09 +0100

    Doc made for AugmentedView/

--- AugmentedView/AugmentedPoiViewController.h
+++ AugmentedView/AugmentedPoiViewController.h
@@ -1,3 +1,8 @@
+/**
+ View representation for an AugmentedPoi
+ @see AugmentedPoi
+ */
+
 //
 //  AugmentedPoiViewController.h
 //  AugmentedMPN
@@ -10,17 +15,25 @@
 
 
 @interface AugmentedPoiViewController : UIViewController <UIScrollViewDelegate>{
-	IBOutlet UILabel *infoLabel;
-	IBOutlet UILabel *subtitleLabel;
-	IBOutlet UIScrollView *tramScroll;
-	IBOutlet UIImageView *arrowImage;
-	IBOutlet UIButton *infoButton;
-	int tramLinesNumber;
-	NSMutableArray *lineViews;
+	IBOutlet UILabel *infoLabel;				/**<Title view */
+	IBOutlet UILabel *subtitleLabel;			/**<Subtitle view */
+	IBOutlet UIScrollView *tramScroll;			/**<Scrolling view for tram line display*/
+	IBOutlet UIImageView *arrowImage;			/**<Pin arrow image */
+	IBOutlet UIButton *infoButton;				/**<Button that triggers info display */
+	int tramLinesNumber;						/**<Number of tram lines for the POI*/
+	NSMutableArray *lineViews;					/**<List of tram number Views */
 }
 
+/** Set the title and subtitle of the view */
 -(void)setTitle:(NSString *)title subtitle:(NSString *)subtitle;
--(void)setArrowLength:(float) length;
+
+/** clear all the tram lines from the view */
 -(void)clearTramLines;
+
+/** add a tram line to the View */
 -(void)addTramLine:(NSString *)name backgroundColor:(UIColor *)backgroundColor foregroundColor:(UIColor *)foregroundColor;
+
+/** Set the length of the pin arrow @deprecated*/
+-(void)setArrowLength:(float) length;
+
 @end
--- AugmentedView/AugmentedViewController.h
+++ AugmentedView/AugmentedViewController.h
@@ -1,3 +1,7 @@
+/**
+ Controls the display of an AugmentedView.
+ */
+
 //
 //  AugmentedViewController.h
 //  AugmentedMPN
@@ -16,16 +20,16 @@
 
 #define HEADING_BUFFER_SIZE 1
 @interface AugmentedViewController : UIViewController <ARViewDelegate>{
-	IBOutlet UIView *poiOverlay;
-	IBOutlet UIView *gridView;
-	AugmentedPoiViewController *infoLabelDisplay;
-	int selectedPoi;
-	NSMutableArray *ar_poiList;
-	NSMutableArray *ar_poiViews;
-	CLLocation *currentLocation;
-	float angleXY;
-	float maxDistance;
-	float minDistance;
+	IBOutlet UIView *poiOverlay;					/**< View container for Poi display */
+	IBOutlet UIView *gridView;						/**< Perspective Grid View */
+	AugmentedPoiViewController *infoLabelDisplay;	/**< Poi call out bubble controller */
+	int selectedPoi;								/**< Currently selected POI, -1 if none is selected */
+	NSMutableArray *ar_poiList;						/**< List containing the POI */
+	NSMutableArray *ar_poiViews;					/**< List containing the POI views */
+	CLLocation *currentLocation;					/**< Current location of the user */
+	float angleXY;									/**< Angle at which the iPhone is held on the XY plane*/
+	float maxDistance;								/**< Distance to the furthest POI */
+	float minDistance;								/**< Distance to the closest POI */
 	int headingBufferIndex;
 	float headingBuffer[HEADING_BUFFER_SIZE];
 }
@@ -34,11 +38,29 @@
 @property (retain) NSMutableArray *ar_poiViews;
 @property (assign)  CLLocation *currentLocation;
 
+/** Moves the perspective grid according to the given orientation
+ @param teta current azimuth of the user from -pi to pi */
 -(void)translateGridWithTeta:(float)teta;
+
+/** Position the given view according to its azimuth and distance to current location
+ @param aView the view to position
+ @param teta azimuth of the POI represented by the view, valued from -pi to pi
+ @param distance distance of the POI represented by the view, must be greater than 0
+ @param scaleEnabled If set to YES, the view is scaled to perspective*/
 -(void)translateView:(UIView *)aView withTeta:(float)teta andDistance:(float)distance withScale:(BOOL)scaleEnabled;
+
+/** Projection from radial coorditates to plane */
 -(float)translationFromAngle:(float)teta;
+
+/** Creates a transform matrix corresponding to the given translation and distance
+ @param translation
+ @param distance must be greater than zero */
 -(CATransform3D)make3dTransformWithTranslation:(float)translation andDistance:(float)distance;
+
+/** Performed when a POI is selected */
 -(void) poiSelected:(id) poiViewId;
+
+/** Add a POI view to the Poi overlay */
 -(void)addPoiView;
 
 @end
